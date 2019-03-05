67581f04c71ba49283ff8830b790b5d3db3c5052
jbisiach <37902505+jbisiach@users.noreply.github.com>
2019-03-05 22:38:28 +0100

    Firebase plugins added

--- app/src/main/java/hkr/da224a/jobshadow/model/Business.java
+++ app/src/main/java/hkr/da224a/jobshadow/model/Business.java
@@ -1,4 +1,72 @@
 package hkr.da224a.jobshadow.model;
 
 public class Business extends User {
+    String businessID;
+    String businessName;
+    String password;
+    String hqAddress;
+    int contactphone;
+    String contactName;
+    String conctactEmail;
+    String website;
+    boolean verified;
+    String dateCreated;
+
+    public Business() {
+    }
+
+    public Business(String businessID, String businessName, String password, String hqAddress,
+                    int contactphone, String contactName, String conctactEmail, String website,
+                    boolean verified, String dateCreated) {
+        this.businessID = businessID;
+        this.businessName = businessName;
+        this.password = password;
+        this.hqAddress = hqAddress;
+        this.contactphone = contactphone;
+        this.contactName = contactName;
+        this.conctactEmail = conctactEmail;
+        this.website = website;
+        this.verified = verified;
+        this.dateCreated = dateCreated;
+    }
+
+    public String getBusinessID() {
+        return businessID;
+    }
+
+    public String getBusinessName() {
+        return businessName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getHqAddress() {
+        return hqAddress;
+    }
+
+    public int getContactphone() {
+        return contactphone;
+    }
+
+    public String getContactName() {
+        return contactName;
+    }
+
+    public String getConctactEmail() {
+        return conctactEmail;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public boolean isVerified() {
+        return verified;
+    }
+
+    public String getDateCreated() {
+        return dateCreated;
+    }
 }
--- app/src/main/java/hkr/da224a/jobshadow/model/Student.java
+++ app/src/main/java/hkr/da224a/jobshadow/model/Student.java
@@ -1,4 +1,97 @@
 package hkr.da224a.jobshadow.model;
 
 public class Student extends User {
+    String studentID;
+    String password;
+    String firstname;
+    String Lastname;
+    int phone;
+    String email;
+    String city;
+    String dob;
+    String emerencyContact;
+    int emergencyContactphone;
+    String degreeProgram;
+    String searchPrefs;
+    String description;
+    String dateCreated;
+
+    public Student() {
+    }
+
+    public Student(String studentID, String password, String firstname, String lastname, int phone,
+                   String email, String city, String dob, String emerencyContact,
+                   int emergencyContactphone, String degreeProgram, String searchPrefs,
+                   String description, String dateCreated) {
+        this.studentID = studentID;
+        this.password = password;
+        this.firstname = firstname;
+        Lastname = lastname;
+        this.phone = phone;
+        this.email = email;
+        this.city = city;
+        this.dob = dob;
+        this.emerencyContact = emerencyContact;
+        this.emergencyContactphone = emergencyContactphone;
+        this.degreeProgram = degreeProgram;
+        this.searchPrefs = searchPrefs;
+        this.description = description;
+        this.dateCreated = dateCreated;
+    }
+
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return Lastname;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public String getEmerencyContact() {
+        return emerencyContact;
+    }
+
+    public int getEmergencyContactphone() {
+        return emergencyContactphone;
+    }
+
+    public String getDegreeProgram() {
+        return degreeProgram;
+    }
+
+    public String getSearchPrefs() {
+        return searchPrefs;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getDateCreated() {
+        return dateCreated;
+    }
 }
