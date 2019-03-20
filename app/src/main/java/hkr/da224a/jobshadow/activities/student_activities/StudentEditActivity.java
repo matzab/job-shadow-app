0400ee1d198b51abdcbff4f0928810a9d9f54537
Hamid Alijani <alijani.hamid@yahoo.com>
2019-03-20 20:49:23 +0100

    notification setting and registration modified

---
+++ app/src/main/java/hkr/da224a/jobshadow/activities/student_activities/StudentEditActivity.java
@@ -0,0 +1,79 @@
+package hkr.da224a.jobshadow.activities.student_activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import hkr.da224a.jobshadow.R;
+import hkr.da224a.jobshadow.activities.ChangePasswordActivity;
+
+public class StudentEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_edit);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setupActionBar();
+
+        Button changePassword = (Button) findViewById(R.id.change_pwd_btn);
+        changePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StudentEditActivity.this, ChangePasswordActivity.class);
+                StudentEditActivity.this.startActivity(intent);
+            }
+        });
+
+    }
+
+    private void setupActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            // Show the Up button in the action bar.
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayShowHomeEnabled(true);
+            actionBar.setTitle("Edit profile");
+        }
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.save_menu_option, menu);
+
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.save_option) {
+
+            // TODO save logic here...
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
